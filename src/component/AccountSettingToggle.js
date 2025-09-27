828286ac416a08566fa5fac3f5c22cedd157cbdd
Litvak Ivan <litvakivan@gmail.com>
2025-09-28 01:02:03 +0300

    resize to mobile version

--- src/component/AccountSettingToggle.js
+++ src/component/AccountSettingToggle.js
@@ -168,8 +168,8 @@ export default function AccountSettingToggle({onProfileEdit}) {
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        width: { xs: 36, sm: 56},
-                        height: { xs: 36, sm: 56},
+                        width: {xs: 36, sm: 56},
+                        height: {xs: 36, sm: 56},
                         borderRadius: '50%',
                         '&:hover': {
                             '& .MuiSvgIcon-root': {
@@ -183,7 +183,7 @@ export default function AccountSettingToggle({onProfileEdit}) {
                 >
                     <Badge badgeContent={unreadCount} color="error">
                         <NotificationsActiveOutlinedIcon sx={{
-                            fontSize: { xs: 28, sm: 40},
+                            fontSize: {xs: 28, sm: 40},
                             transition: 'color 0.5s ease'
                         }}/>
                     </Badge>
@@ -196,12 +196,19 @@ export default function AccountSettingToggle({onProfileEdit}) {
                     aria-haspopup="true"
                     onClick={handleProfileMenuOpen}
                     color="inherit"
-                    sx={{mr: {xs: 0, sm: 1}}}
+                    sx={{
+                        width: {xs: 36, sm: 56},
+                        height: {xs: 36, sm: 56},
+                        mr: {xs: 0, sm: 1}
+                    }}
                 >
                     <Avatar
-                        alt="Remy Sharp"
+                        alt="Avatar"
                         src={avatarSrc}
-                        sx={{width: { xs: 26, sm: 40}, height: { xs: 26, sm: 40}}}
+                        sx={{
+                            width: {xs: 26, sm: 40},
+                            height: {xs: 26, sm: 40}
+                        }}
                     />
                 </IconButton>
             </Box>
--- src/component/TopMenu.js
+++ src/component/TopMenu.js
@@ -3,18 +3,20 @@ import AccountSettingToggle from "./AccountSettingToggle";
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Item from "./StyledItem";
-import Logo2 from "./Logo2";
+import Logo from "./Logo";
 
 function TopMenu({onProfileEdit}) {
     return (
         <Grid container size={{xs: 12, sm: 12}} spacing={0}>
             <Grid size={{xs: 2, sm: 2}}>
                 <Item noshadow>
-                    <Logo2/>
+                    <Logo/>
                 </Item>
             </Grid>
             <Grid size={{xs: 6, sm: 7}}>
-                <Item noshadow><SearchLine/></Item>
+                <Item noshadow>
+                    <SearchLine/>
+                </Item>
             </Grid>
             <Grid size={{xs: 4, sm: 3}}>
                 <Item noshadow>
