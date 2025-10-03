246027b6e750d58e20c34d6e3010870528841907
Litvak Ivan <litvakivan@gmail.com>
2025-10-03 11:25:46 +0300

    resize refactor menu design

--- src/http/HttpClient.js
+++ src/http/HttpClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://192.168.1.141:9000/api/v1/',
+    // baseURL: 'http://192.168.1.141:9000/api/v1/',
+    baseURL: 'https://api.wishlisty.ru/api/v1/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
